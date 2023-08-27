78aafd79c1d7706af458317031c7ab29813ccb44
Girish019 <118633556+Girish019@users.noreply.github.com>
2023-08-27 17:27:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,10 @@ from logging.handlers import RotatingFileHandler
 
 
 
-#Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5400122847:AAF24xA9BdA59VwOt6dMeb9WOyYaKAom-2w")
-
+#Bot token @Botfather 6102959656:AAGNVH5A8IHdGs8fSb7uoVkOT-FHWVXRgwE
+#TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6102959656:AAGNVH5A8IHdGs8fSb7uoVkOT-FHWVXRgwE") #miror leech
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6180766269:AAF0jG2fxEc6iu19Ps8HFAQYa3lmQbtyTeg") #new fs
+#TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5946382591:AAE6eSYjUSUyfMgACi5zudlfZcdzVRxiSJU") #copy contact 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "10755921"))
 
@@ -19,8 +20,8 @@ APP_ID = int(os.environ.get("APP_ID", "10755921"))
 API_HASH = os.environ.get("API_HASH", "d5e49fd3637cba407f17807d31c77977")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001790801242"))
-LOG_ID = int(os.environ.get("LOG_ID", "-1001657067333"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001842556179"))
+LOG_ID = int(os.environ.get("LOG_ID", "-1001748750847"))
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "5636224141"))
 
@@ -32,7 +33,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Chandan:Chandan@cluster0.
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001798620901"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", 0))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "15"))
 
