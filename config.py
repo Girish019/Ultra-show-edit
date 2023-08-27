db594a95a007981d36c676d939b80329f86ac50b
Girish019 <118633556+Girish019@users.noreply.github.com>
2023-08-27 17:15:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Chandan:Chandan@cluster0.2lauy.mongodb.net/cluster0?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "TKHfsbot")
+DB_NAME = os.environ.get("DATABASE_NAME", "PROFS30M")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001588924239"))
