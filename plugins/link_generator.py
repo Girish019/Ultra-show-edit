8d5fc509cd9e43144aca676706752c6625b72829
Girish019 <118633556+Girish019@users.noreply.github.com>
2023-09-05 10:34:46 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -61,7 +61,7 @@ async def link_generator(client: Client, message: Message):
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     await channel_message.reply_text(f"<b>Here is your link</b>\n\n{link}\n\n<code>{link}</code>", quote=True, reply_markup=reply_markup)
 
-async def tlinkgen(message: Message):
+async def tlinkgen(client: Client, message: Message):
   #  reply_text = await message.reply_text("Please Wait...!", quote = True)
     try:
         post_msg = await message.copy(chat_id=CHANNEL_ID, disable_notification=True)
@@ -72,7 +72,7 @@ async def tlinkgen(message: Message):
         print(e)
     await asyncio.sleep(5)
     base64_string = await encode(f"get-{post_msg.id * abs(CHANNEL_ID)}")
- #   link = f"https://telegram.me/{client.username}?start={base64_string}"
-    link = f"https://telegram.me/Newfs2bot?start={base64_string}"
+    link = f"https://telegram.me/{client.username}?start={base64_string}"
+ #   link = f"https://telegram.me/Newfs2bot?start={base64_string}"
     return link
     
